688e24c73df51cce19774dfdbd2950ecc149c151
emelygap <mily18gaby@outlook.com>
2023-04-04 22:17:56 +0200

    Añadida la vista y configuración de las prendas a cada Activity de cada cajón

--- app/src/main/java/com/example/proyectofinal_frame1/PrendaAdapter.java
+++ app/src/main/java/com/example/proyectofinal_frame1/PrendaAdapter.java
@@ -1,2 +1,71 @@
-package com.example.proyectofinal_frame1;public class PrendaAdapter {
+package com.example.proyectofinal_frame1;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class PrendaAdapter extends RecyclerView.Adapter<PrendaAdapter.PrendaViewHolder>{
+    private List<Prenda> prendas;
+
+    public PrendaAdapter(List<Prenda> prendas) {
+        this.prendas = prendas;
+    }
+
+    @NonNull
+    @Override
+    public PrendaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_prenda, parent, false);
+        return new PrendaViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PrendaViewHolder holder, int position) {
+        Prenda prenda = prendas.get(position);
+        holder.bind(prenda);
+    }
+
+    @Override
+    public int getItemCount() {
+        return prendas.size();
+    }
+
+    public static class PrendaViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView imageView;
+        private TextView nombreTextView;
+        private TextView textViewEtiqueta1;
+        private TextView textViewEtiqueta2;
+        private TextView textViewEtiqueta3;
+
+        public PrendaViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageViewPrenda);
+            nombreTextView = itemView.findViewById(R.id.textViewNombrePrenda);
+            textViewEtiqueta1 = itemView.findViewById(R.id.textViewEtiqueta1);
+            textViewEtiqueta2 = itemView.findViewById(R.id.textViewEtiqueta2);
+            textViewEtiqueta3 = itemView.findViewById(R.id.textViewEtiqueta3);
+        }
+
+        public void bind(Prenda prenda) {
+            imageView.setImageResource(prenda.getUrlImagen());
+            nombreTextView.setText(prenda.getNombre());
+            List<String> etiquetas = prenda.getEtiquetas();
+            if (etiquetas.size() > 0) {
+                textViewEtiqueta1.setText(etiquetas.get(0));
+            }
+            if (etiquetas.size() > 1) {
+                textViewEtiqueta2.setText(etiquetas.get(1));
+            }
+            if (etiquetas.size() > 2) {
+                textViewEtiqueta3.setText(etiquetas.get(2));
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/proyectofinal_frame1/ZapatosActivity.java
+++ app/src/main/java/com/example/proyectofinal_frame1/ZapatosActivity.java
@@ -1,14 +1,36 @@
 package com.example.proyectofinal_frame1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ZapatosActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewPrendas;
+    private PrendaAdapter prendaAdapter;
+    private List<Prenda> listaPrendas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_zapatos);
+        setContentView(R.layout.activity_ropa_superior);
+
+        recyclerViewPrendas = findViewById(R.id.recyclerViewPrendas);
+        recyclerViewPrendas.setLayoutManager(new GridLayoutManager(this, 2));
+
+        listaPrendas = new ArrayList<>();
+        listaPrendas.add(new Prenda("Sandalias", R.drawable.ic_baseline_photo_camera_back_24, Arrays.asList("Formal", "Blanco", "Verano")));
+        listaPrendas.add(new Prenda("Chanclas", R.drawable.ic_baseline_photo_camera_back_24, Arrays.asList("Informal", "Azul")));
+        listaPrendas.add(new Prenda("Botas", R.drawable.ic_baseline_photo_camera_back_24, Arrays.asList("Formal", "Negro", "Invierno")));
+        listaPrendas.add(new Prenda("Zapatillas", R.drawable.ic_baseline_photo_camera_back_24, Arrays.asList("Informal", "Azul")));
+
+        prendaAdapter = new PrendaAdapter(listaPrendas);
+        recyclerViewPrendas.setAdapter(prendaAdapter);
     }
 }
