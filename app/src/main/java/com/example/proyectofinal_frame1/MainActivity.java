b35390a35072c3d5d09f1396bb7ca2d57a6239af
Paula-VF <paulavallespinforner@gmail.com>
2023-04-09 14:59:25 +0200

    Toolbar e iconos mas grandes

--- app/src/main/java/com/example/proyectofinal_frame1/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal_frame1/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,6 +15,7 @@ import com.example.proyectofinal_frame1.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar myToolBar;
     private ActivityMainBinding binding;
 
     @Override
@@ -22,6 +24,22 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // para ocultar la barra superior que viene por defecto
+        // getSupportActionBar().hide();
+
+        // para que se muestre nuestra barra superior:
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        // setSupportActionBar(myToolBar);
+        /*
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_baseline_add_24);
+        getSupportActionBar().setIcon(R.drawable.ic_baseline_filter_alt_24);
+        getSupportActionBar().setIcon(R.drawable.ic_baseline_search_24);
+         */
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
