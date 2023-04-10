cf3e408bff841f1a9a6b1d497d62dd74c8901e8d
Paula-VF <paulavallespinforner@gmail.com>
2023-04-10 14:26:30 +0200

    Toolbar e iconos mas grandes

--- app/src/main/java/com/example/proyectofinal_frame1/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal_frame1/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.proyectofinal_frame1;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
@@ -15,7 +19,7 @@ import com.example.proyectofinal_frame1.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar myToolBar;
+    private Toolbar myToolbar;
     private ActivityMainBinding binding;
 
     @Override
@@ -29,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         // getSupportActionBar().hide();
 
         // para que se muestre nuestra barra superior:
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
         // setSupportActionBar(myToolBar);
@@ -45,11 +49,31 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.carouselFragment, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
+    // para que aparezcan los iconos de toolbar_prendas en el toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_prendas, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.AddBtn){
+            Toast.makeText(this, "Añadir", Toast.LENGTH_SHORT).show();
+        } else if(id == R.id.FilterBtn){
+            Toast.makeText(this, "Filtrar", Toast.LENGTH_SHORT).show();
+        } else if(id == R.id.SearchBtn){
+            Toast.makeText(this, "Buscar", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
 }
