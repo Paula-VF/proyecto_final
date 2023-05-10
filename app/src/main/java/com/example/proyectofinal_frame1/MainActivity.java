5208e9418f9a235404b16c187a295de72a86ef89
emelygap <mily18gaby@outlook.com>
2023-05-10 03:06:52 +0200

    Galería y cámara se abren, falta añadir las imagenes a la bd

--- app/src/main/java/com/example/proyectofinal_frame1/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal_frame1/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,6 +24,9 @@ import android.widget.Toast;
 
 import com.example.proyectofinal_frame1.database.ProyectoDatabaseHelper;
 
+import com.example.proyectofinal_frame1.database.TablaCategoria;
+import com.example.proyectofinal_frame1.database.TablaPrenda;
+import com.example.proyectofinal_frame1.database.TablaUsuario;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.activity.result.ActivityResult;
@@ -34,6 +40,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -42,14 +49,15 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.proyectofinal_frame1.databinding.ActivityMainBinding;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 //  implements View.OnClickListener
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar myToolbar;
-    public static final int PICK_IMAGE = 1; // para saber cuando el usuario elige una foto
-    static final int REQUEST_IMAGE_CAPTURE = 1; // para saber cuando el usuario toma una foto
-    private static final int CAMERA_REQUEST = 1;
+
     ActivityResultLauncher<Intent> resultLauncher;
     private ImageView imagen;
     private ActivityMainBinding binding;
@@ -74,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(myToolbar);
 
         imagen = (ImageView) findViewById(R.id.imagen);
-        registerResult();
+//        registerResult(); Esto creo que se tiene que borrar
 
         // categorías
         imageButtonArriba = (ImageButton) findViewById(R.id.imageButtonArriba);
@@ -134,12 +142,11 @@ public class MainActivity extends AppCompatActivity {
 //        TablaUsuario user = new TablaUsuario(this);
 //
 //        String rutaImagen = "https://www.trajesguzman.com/media/1624/camisa-basica-blanca.jpg";
-//        long idCategoria = categoria.insertarCategoria("superior");
 //        long idUser = user.insertarUsuario("emely", "mijij", "okey");
-//        long id= prenda.insertarPrenda("Camisa", rutaImagen, "verano", idCategoria, idUser);
-//        long id2= prenda.insertarPrenda("Camisa", "https://hmperu.vtexassets.com/arquivos/ids/3024520-483-725/Pantalon-de-buzo---Rosado---H-M-PE.jpg?v=637983998116170000", "verano", idCategoria, idUser);
+//        long id= prenda.insertarPrenda("Camisa", rutaImagen, "verano", 1, idUser);
+//        long id2= prenda.insertarPrenda("Camisa", "https://hmperu.vtexassets.com/arquivos/ids/3024520-483-725/Pantalon-de-buzo---Rosado---H-M-PE.jpg?v=637983998116170000", "verano", 1, idUser);
 
-        //
+
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -153,8 +160,9 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
-
-
+    public static final int PICK_IMAGE = 2; // para saber cuando el usuario elige una foto
+    static final int REQUEST_IMAGE_CAPTURE = 1; // para saber cuando el usuario toma una foto
+    Bitmap bitmap;
     // para que aparezcan los iconos de toolbar_prendas en el toolbar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -162,36 +170,32 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    // funcionalidad botones del TOOLBAR
+
+    String currentPhotoPath;
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
+
+        //Añadir foto
 
         if(id == R.id.AddBtn){
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
             View dView = getLayoutInflater().inflate(R.layout.seleccionar_imagen, null);
             Button camara = (Button) dView.findViewById(R.id.btnCamara);
             Button galeria = (Button) dView.findViewById(R.id.btnGaleria);
+
             camara.setOnClickListener(new View.OnClickListener() {
                 @RequiresApi(api = Build.VERSION_CODES.O)
                 @Override
                 public void onClick(View v) {
-                    if(!checkCameraPermission()){
-                        requestCameraPermission();
-                    }else{
-                        takePicture();
-                    }
+                    camaraLauncher.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
                 }
             });
             galeria.setOnClickListener(new View.OnClickListener() {
                 @RequiresApi(api = Build.VERSION_CODES.O)
                 @Override
                 public void onClick(View v) {
-                    if(!checkStoragePermission()){
-                        requestStoragePermission();
-                    }else{
-                        pickImage();
-                    }
+                    galeriaLauncher.launch("image/*");
                 }
             });
 
@@ -206,113 +210,46 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    private boolean checkCameraPermission(){
-        boolean res1 = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED;
-        boolean res2 = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_MEDIA_IMAGES)== PackageManager.PERMISSION_GRANTED;
-        return res1 && res2;
-    }
-
-    private boolean checkStoragePermission(){
-        boolean res2 = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_MEDIA_IMAGES)== PackageManager.PERMISSION_GRANTED;
-        return res2;
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void requestCameraPermission(){
-        requestPermissions(new String[]{android.Manifest.permission.CAMERA, android.Manifest.permission.READ_MEDIA_IMAGES}, 100);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void requestStoragePermission(){
-        requestPermissions(new String[]{android.Manifest.permission.READ_MEDIA_IMAGES}, 100);
-        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_MEDIA_IMAGES)== PackageManager.PERMISSION_GRANTED){
-            pickImage();
+    ActivityResultLauncher<Intent> camaraLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if(result.getResultCode()==RESULT_OK){
+                Bundle extras = result.getData().getExtras();
+                Bitmap imgBitmap = (Bitmap) extras.get("data");
+                imagen.setImageBitmap(imgBitmap);
+            }
         }
-    }
+    });
 
-    private void registerResult(){
-        resultLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if(result.getResultCode() == Activity.RESULT_OK) {
-                            try {
-                                Uri uri = result.getData().getData();
-                                /*
-                                final String path = getPathFromUri(uri);
-                                if(path!=null){
-                                    File f = new File(path);
-                                    uri = Uri.fromFile(f);
-                                }
-                                */
-
-                                //imagen.setImageURI(uri);
-                                Intent intent = new Intent(MainActivity.this,  ImagenSeleccionada.class);
-                                intent.putExtra("imageUri", uri.toString());
-                                //intent.setData(uri);
-                                //intent.setType("image/*");
-                                //intent.setAction(Intent.ACTION_GET_CONTENT);
-                                startActivity(intent);
-                            } catch (Exception e) {
-                                Toast.makeText(MainActivity.this, "Ninguna imagen seleccionada", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    }
-                });
-    }
-
-    public String getPathFromUri(Uri uri){
-        String res = null;
-        String[] proj ={MediaStore.Images.Media.DATA};
-        Cursor cursor = getContentResolver().query(uri,proj,null,null,null);
-        if(cursor.moveToFirst()){
-            int colum_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            res = cursor.getString(colum_index);
+    ActivityResultLauncher<String> galeriaLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+        @Override
+        public void onActivityResult(Uri result) {
+            imagen.setImageURI(result);
         }
-        cursor.close();
+    });
 
-        return res;
-    }
-
-    private void pickImage(){
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        //Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        //intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("image/*");
-        resultLauncher.launch(intent);
-    }
-
-    /*
-    private void registerResultCamara(Intent intentCamara){
-        resultLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        super.onActivityResult(requestCode, resultCode, data);
-                        try {
-                            if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
-                                Bundle extras = result.getData().getExtras();
-                                Bitmap photo = (Bitmap) intentCamara.getExtras().get("data");
-                                imagen.setImageBitmap(photo);
-                                Intent intent = new Intent(MainActivity.this, ImagenSeleccionada.class);
-                                // intent.setData(photo);
-                                startActivity(intent);
-                            }
-                        } catch (Exception e) {
-                            Toast.makeText(MainActivity.this, "Ninguna imagen seleccionada", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-
-     */
-
-    private void takePicture(){
-        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.setType("image/*");
-    }
-
+//    private boolean checkCameraPermission(){
+//        boolean res1 = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED;
+//        boolean res2 = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_MEDIA_IMAGES)== PackageManager.PERMISSION_GRANTED;
+//        return res1 && res2;
+//    }
+//
+//    private boolean checkStoragePermission(){
+//        boolean res2 = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_MEDIA_IMAGES)== PackageManager.PERMISSION_GRANTED;
+//        return res2;
+//    }
+//
+//    @RequiresApi(api = Build.VERSION_CODES.O)
+//    private void requestCameraPermission(){
+//        requestPermissions(new String[]{android.Manifest.permission.CAMERA, android.Manifest.permission.READ_MEDIA_IMAGES}, 100);
+//    }
+//
+//    @RequiresApi(api = Build.VERSION_CODES.O)
+//    private void requestStoragePermission(){
+//        requestPermissions(new String[]{android.Manifest.permission.READ_MEDIA_IMAGES}, 100);
+//        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_MEDIA_IMAGES)== PackageManager.PERMISSION_GRANTED){
+//            pickImage();
+//        }
+//    }
 
 }
